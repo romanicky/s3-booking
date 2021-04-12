2818f75d4bb61c40c68b3d9a4843c612e8efda54
Roman Tran <ppt@globeteam.com>
2021-04-13 00:38:30 +0700

    init newapp

--- src/App.js
+++ src/App.js
@@ -1,6 +1,25 @@
-// ** Router Import
-import Router from './router/Router'
+import logo from './logo.svg';
+import './App.css';
 
-const App = props => <Router />
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
 
-export default App
+export default App;
